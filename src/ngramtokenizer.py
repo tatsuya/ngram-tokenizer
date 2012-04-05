49517c6f2c01f33e87f78263702ace73a13a70e1
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2012-04-04 17:03:08 -0700

    third commit

--- src/ngramtokenizer.py
+++ src/ngramtokenizer.py
@@ -1,20 +1,43 @@
 # -*- coding: utf-8 -*-
 
-class NgramTokenizer(object):
+'''
+Created on 2012/03/23
 
-    def tokenize(self, text, n=1):
-        results = []
-        for chunk in text.split(' '):
-            if len(chunk) >= n:
-                for i in xrange(len(chunk) - n + 1):
-                    results.append(chunk[i:i + n])
-            else:
-                results.append(chunk)
+@author: tatsuya.oiwa
+'''
 
-        return results
+from optparse import OptionParser
+
+def tokenize(text, n):
+    '''Tokenize a text.
+    
+    Args:
+      text:
+        The text for tokenize.
+      n:
+        The number of N-gram.
+    '''
+    for chunk in text.split(' '):
+        if len(chunk) > n:
+            for i in xrange(len(chunk) - n + 1):
+                print chunk[i:i + n] + '|',
+        else:
+            print chunk
 
 if __name__ == '__main__':
-    nt = NgramTokenizer()
-    results = nt.tokenize('This is a pen.', 2)
-    for chunk in results:
-        print chunk + ' |',
+    usage = 'usage: %prog [options]'
+    parser = OptionParser(usage)
+    
+    parser.add_option('-t', '--text', action='store', type='string',
+                      metavar='<text>', default='Hello world!',
+                      dest='text', help='')
+    
+    parser.add_option('-n', action='store', type='int',
+                      metavar='<number of ngram>', default='3',
+                      dest='n', help='')
+    
+    args = parser.parse_args()
+    options = args[0]
+    
+    text = options.text.decode('utf-8').encode('utf-8')
+    tokenize(text.decode('utf-8'), int(options.n))
